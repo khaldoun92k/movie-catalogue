d32d1ead409a9b8a53ca3c357303ce63507a4b05
khaldoun92k <khaldoun92k@gmail.com>
2023-07-31 15:19:39 +0200

    initial spring security push

--- src/main/java/com/movie/MovieCatalogueApplication.java
+++ src/main/java/com/movie/MovieCatalogueApplication.java
@@ -1,11 +1,27 @@
 package com.movie;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieCatalogueApplication {
 
+	//Adding Dynamic Content to angular
+	  @RequestMapping("/resource")
+	  public Map<String,Object> home() {
+	    Map<String,Object> model = new HashMap<String,Object>();
+	    model.put("id", UUID.randomUUID().toString());
+	    model.put("content", "Hello World");
+	    return model;
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueApplication.class, args);
 	}
--- src/main/java/com/movie/controllers/UserController.java
+++ src/main/java/com/movie/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
